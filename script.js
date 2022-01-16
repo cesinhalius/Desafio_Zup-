31e85c378481c71e5f75eb19f115795f2776c83e
Cesar Augusto <cesinhalius@hotmail.com>
2022-01-16 11:23:50 -0300

    Implementando o sorteio de personagens e conexão com a API

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ const Persongem = document.querySelector("#personagens");
 
 gerarValorAleatorio = () =>{
     return Math.floor(Math.random() * 671);
-}
+};
+
+let getPersonagens = async(total = 3) =>{
+    let personagensID = [];
+    for(let i =0; i < total; i++){
+        // Aqui ele faz o sorteio do personagem que vai pegar
+        personagensID.push(gerarValorAleatorio());
+    }
+
+    const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${personagensID}`,
+        {
+            method:"GET",
+            headers: {
+                Accept: " application/json ",
+                " Content-Type ": "application/json",
+            },
+        }
+    );
+
+    return await response.json();
+
+};
