aeb29d98a7cf77adc8a713970494e25b7af61896
Cesar Augusto <cesinhalius@hotmail.com>
2022-01-16 11:12:39 -0300

    Implementando a função de gerar um numero do personagem

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao = documemt.querySelector("button");
+const Persongem = document.querySelector("#personagens");
+
+gerarValorAleatorio = () =>{
+    return Math.floor(Math.random() * 671);
+}
