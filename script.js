b2b4256cf956005f5dbe03026596c935d45e56f6
Cesar Augusto <cesinhalius@hotmail.com>
2022-01-16 11:42:06 -0300

    Implementando a função de listagem de personagens

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 const botao = documemt.querySelector("button");
 const Persongem = document.querySelector("#personagens");
 
-gerarValorAleatorio = () =>{
-    return Math.floor(Math.random() * 671);
+gerarValorAleatorio = () => {
+  return Math.floor(Math.random() * 671);
 };
 
-let getPersonagens = async(total = 3) =>{
-    let personagensID = [];
-    for(let i =0; i < total; i++){
-        // Aqui ele faz o sorteio do personagem que vai pegar
-        personagensID.push(gerarValorAleatorio());
+let getPersonagens = async (total = 3) => {
+  let personagensID = [];
+  for (let i = 0; i < total; i++) {
+    // Aqui ele faz o sorteio do personagem que vai pegar
+    personagensID.push(gerarValorAleatorio());
+  }
+
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${personagensID}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: " application/json ",
+        " Content-Type ": "application/json",
+      },
     }
+  );
 
-    const response = await fetch(
-        `https://rickandmortyapi.com/api/character/${personagensID}`,
-        {
-            method:"GET",
-            headers: {
-                Accept: " application/json ",
-                " Content-Type ": "application/json",
-            },
-        }
-    );
-
-    return await response.json();
-
+  return await response.json();
 };
+
+pegarListadePersonagem = async () => {
+  let html = "";
+  let p = await getPersonagens();
+
+  p.forEach((personagem) => {
+    const { name, image, species, status } = personagem;
+
+    html += `<img alt="${image}" src=${image} >`;
+    html += `<ul id="detalhes-container>
+                <li>Nome:<p id="nome">${name}</p></li>
+                <li>Espécie:<p id="especie">${species}</p></li>
+                <li>Esta Vivo?:<p id="condicao">${name}</p></li>
+        </ul>`;
+  });
+
+  Persongem.innerHTML = html;
+};
+
+botao.onclick = pegarListadePersonagem;
